8e724f97c0d9e0f0eaa5062e02e6deccf3fc0bbc
zxcvbrnz <muhammadsabirin2004@gmail.com>
2026-05-19 17:46:17 +0800

    Fix course package retrieval to ensure the latest packages are fetched correctly

--- app/Livewire/LearningList.php
+++ app/Livewire/LearningList.php
@@ -10,7 +10,7 @@ class LearningList extends Component
     public function render()
     {
         // Mengambil semua paket kursus milik user melalui relasi pivot dengan yang terbaru paling atas
-        $myCourses = Auth::user()->coursePackages()->latest()->get();
+        $myCourses = Auth::user()->coursePackages->latest();
 
         return view('livewire.learning-list', [
             'myCourses' => $myCourses
