7772975b69830cc6877420c35fc3a12db044a12a
zxcvbrnz <muhammadsabirin2004@gmail.com>
2026-05-19 17:47:57 +0800

    ---

--- app/Livewire/LearningList.php
+++ app/Livewire/LearningList.php
@@ -10,7 +10,7 @@ class LearningList extends Component
     public function render()
     {
         // Mengambil semua paket kursus milik user melalui relasi pivot dengan yang terbaru paling atas
-        $myCourses = Auth::user()->coursePackages->latest();
+        $myCourses = Auth::user()->coursePackages()->latest()->get();
 
         return view('livewire.learning-list', [
             'myCourses' => $myCourses
