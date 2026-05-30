f84d63dd718f12ff87fcf30ab1be099262fdcbe0
zxcvbrnz <muhammadsabirin2004@gmail.com>
2026-05-30 14:01:56 +0800

    Update page title to reflect brand identity as Cenari ID

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Cenari Education Center - Konvergensi Teknologi 2026</title>
+    <title>Cenari ID - Rancang Masa Depanmu</title>
 
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="shortcut icon" href="{{ asset('logoCenari2020 PATEN.png') }}" type="image/x-icon">
