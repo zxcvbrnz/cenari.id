0b8830780d857b08decdc4c754fbed49faf488d2
zxcvbrnz <muhammadsabirin2004@gmail.com>
2026-06-06 13:58:37 +0800

    ----

--- resources/views/livewire/b2-b-solution.blade.php
+++ resources/views/livewire/b2-b-solution.blade.php
@@ -77,8 +77,8 @@
                         <div>
                             <div class="bg-slate-900 rounded-[2rem] p-8 text-white flex flex-col items-start">
                                 <span
-                                    class="text-[10px] font-black text-blue-400 uppercase tracking-widest block mb-4">Expected
-                                    Learning Outcome</span>
+                                    class="text-[10px] font-black text-blue-400 uppercase tracking-widest block mb-4">Target
+                                    Hasil Belajar</span>
                                 <p class="text-lg leading-relaxed italic">"{{ $data->outcome }}"</p>
                             </div>
                         </div>
