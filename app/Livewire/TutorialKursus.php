e4b8acf0e880eefd333dc0d67b0e5eab2fb07579
zxcvbrnz <muhammadsabirin2004@gmail.com>
2026-06-07 17:53:37 +0800

    -

--- app/Livewire/TutorialKursus.php
+++ app/Livewire/TutorialKursus.php
@@ -10,7 +10,7 @@ class TutorialKursus extends Component
     public $tab = 'instruktur';
 
     // Menyimpan ID video YouTube tutorial (Ganti dengan ID asli video Anda)
-    public $youtubeVideoId = 'hrEVIU25fqY';
+    public $youtubeVideoId = 'hrEVlU25fqY';
 
     public function setTab($role)
     {
