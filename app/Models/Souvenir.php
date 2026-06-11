26bef2ab0ddc70aefad23374655b3e9686b8522e
zxcvbrnz <muhammadsabirin2004@gmail.com>
2026-06-11 17:44:34 +0800

    feat: implement Souvenir and SouvenirImage models with relationships and fillable attributes

--- app/Models/Souvenir.php
+++ app/Models/Souvenir.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Souvenir extends Model
 {
-    //
+    protected $fillable = ['name', 'price', 'description'];
+
+    public function images()
+    {
+        return $this->hasMany(SouvenirImage::class);
+    }
 }
--- app/Models/SouvenirImage.php
+++ app/Models/SouvenirImage.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class SouvenirImage extends Model
 {
-    //
+    protected $fillable = ['souvenir_id', 'filename', 'is_featured'];
+
+    public function souvenir()
+    {
+        return $this->belongsTo(Souvenir::class);
+    }
 }
